e6f554cbe09671b954f89a9dc80171ae5c953a52
ajohnson710 <aaronj047@gmail.com>
2024-11-22 18:00:02 -0500

    idek

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from 'express'
 import Hello from './Hello.js'
 import Lab5 from './Lab5/index.js'
+import cors from 'cors'
+import UserRoutes from "./Kanbas/Users/routes.js";
 
 const app = express()
+UserRoutes(app);
+
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}))
+app.use(express.json())
 Lab5(app)
 Hello(app)
-app.listen(4000)
-
+app.listen(process.env.PORT || 4000)
