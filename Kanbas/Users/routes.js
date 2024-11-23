24d4d7dc792429a6e6a3234db395ab622be675f0
ajohnson710 <aaronj047@gmail.com>
2024-11-22 21:16:59 -0500

    a5 enough

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -1,6 +1,16 @@
 import * as dao from "./dao.js";
-let currentUser = null;
+import * as courseDao from "../Courses/dao.js";
+import * as enrollmentsDao from "../Enrollments/dao.js";
+
 export default function UserRoutes(app) {
+    const createCourse = (req, res) => {
+        const currentUser = req.session["currentUser"];
+        const newCourse = courseDao.createCourse(req.body);
+        enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
+        res.json(newCourse);
+    };
+    app.post("/api/users/current/courses", createCourse);
+
     const createUser = (req, res) => { };
     const deleteUser = (req, res) => { };
     const findAllUsers = (req, res) => { };
@@ -9,9 +19,11 @@ export default function UserRoutes(app) {
         const userId = req.params.userId;
         const userUpdates = req.body;
         dao.updateUser(userId, userUpdates);
-        currentUser = dao.findUserById(userId);
+        const currentUser = dao.findUserById(userId);
+        req.session["currentUser"] = currentUser;
         res.json(currentUser);
     };
+
     const signup = (req, res) => {
         const user = dao.findUserByUsername(req.body.username);
         if (user) {
@@ -19,18 +31,54 @@ export default function UserRoutes(app) {
                 { message: "Username already in use" });
             return;
         }
-        currentUser = dao.createUser(req.body);
-        res.json(currentUser);
-    };
-    const signin = (req, res) => {
-        console.log(req.body);
-        const { username, password } = req.body;
-        currentUser = dao.findUserByCredentials(username, password);
+        const currentUser = dao.createUser(req.body);
+        req.session["currentUser"] = currentUser;
         res.json(currentUser);
     };
 
-    const signout = (req, res) => { };
-    const profile = (req, res) => { };
+
+    const signin = (req, res) => {
+        const { username, password } = req.body;
+        const currentUser = dao.findUserByCredentials(username, password);
+        if (currentUser) {
+            req.session["currentUser"] = currentUser;
+            res.json(currentUser);
+        } else {
+            res.status(401).json({ message: "Unable to login. Try again later." });
+        }
+    };
+
+    const profile = async (req, res) => {
+        const currentUser = req.session["currentUser"];
+        if (!currentUser) {
+            res.sendStatus(401);
+            return;
+        }
+        res.json(currentUser);
+    };
+
+
+    const signout = (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    };
+
+    const findCoursesForEnrolledUser = (req, res) => {
+        let { userId } = req.params;
+        if (userId === "current") {
+            const currentUser = req.session["currentUser"];
+            if (!currentUser) {
+                res.sendStatus(401);
+                return;
+            }
+            userId = currentUser._id;
+        }
+        const courses = courseDao.findCoursesForEnrolledUser(userId);
+        res.json(courses);
+    };
+    app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
+
+
     app.post("/api/users", createUser);
     app.get("/api/users", findAllUsers);
     app.get("/api/users/:userId", findUserById);
--- index.js
+++ index.js
@@ -3,16 +3,39 @@ import Hello from './Hello.js'
 import Lab5 from './Lab5/index.js'
 import cors from 'cors'
 import UserRoutes from "./Kanbas/Users/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import session from "express-session";
+import "dotenv/config";
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
 
 const app = express()
-UserRoutes(app);
-
 
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000'
 }))
+app.use(session(sessionOptions));
+
 app.use(express.json())
+
 Lab5(app)
 Hello(app)
+UserRoutes(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+
 app.listen(process.env.PORT || 4000)
